75b5296b690d602231946eb43fc5e3d50ce4b2ea
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-21 02:02:40 -0300

    ✨ Feat: Added recent-blogs and fixed home routing

---
+++ src/app/modules/home/components/recent-blog-entries/recent-blog-entries.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-blog-entries',
+  templateUrl: './recent-blog-entries.component.html',
+  styleUrls: ['./recent-blog-entries.component.scss']
+})
+export class RecentBlogEntriesComponent {
+
+}
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
+import { RecentBlogEntriesComponent } from './components/recent-blog-entries/recent-blog-entries.component';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HomeComponent, RecentBlogEntriesComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
