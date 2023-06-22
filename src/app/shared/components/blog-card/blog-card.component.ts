42b2d311a0cdffd3732d871e08a6f1356a0d46b7
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-22 19:33:30 -0300

    ✨ Feat: Added blog card data binding

--- src/app/shared/components/blog-card/blog-card.component.ts
+++ src/app/shared/components/blog-card/blog-card.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Blog } from 'src/app/models/blog.interface';
 
 @Component({
   selector: 'app-blog-card',
   templateUrl: './blog-card.component.html',
-  styleUrls: ['./blog-card.component.scss']
+  styleUrls: ['./blog-card.component.scss'],
 })
 export class BlogCardComponent {
-
+  @Input() blog: Blog = {
+    title: '',
+    author: '',
+    createdAt: new Date(),
+    image: '',
+    tags: [],
+    description: '',
+    content: '',
+    pages: [],
+  };
 }
