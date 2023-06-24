61607b91ef56f2790fabcac501763d73f6d621fc
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-24 01:04:26 -0300

    ✨ Feat: Added blog content and header components

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -4,9 +4,17 @@ import { BlogComponent } from './blog.component';
 import { BlogRoutingModule } from './blog-routing.module';
 import { BlogEntryComponent } from './components/blog-entry/blog-entry.component';
 import { BlogScrollerComponent } from './components/blog-scroller/blog-scroller.component';
+import { BlogHeaderComponent } from './components/blog-entry/components/blog-header/blog-header.component';
+import { BlogContentComponent } from './components/blog-entry/components/blog-content/blog-content.component';
 
 @NgModule({
-  declarations: [BlogComponent, BlogEntryComponent, BlogScrollerComponent],
+  declarations: [
+    BlogComponent,
+    BlogEntryComponent,
+    BlogScrollerComponent,
+    BlogHeaderComponent,
+    BlogContentComponent,
+  ],
   imports: [CommonModule, BlogRoutingModule],
 })
 export class BlogModule {}
