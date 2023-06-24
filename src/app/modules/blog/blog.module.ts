ecca5bc1ae9c0ff568d572902de77a75161ae932
Uriel Spiridione <urielspiridione135@gmail.com>
2023-06-24 00:49:44 -0300

    ✨ Feat: Added BlogEntry and BlogSrcoller Components

--- src/app/modules/blog/blog.module.ts
+++ src/app/modules/blog/blog.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogComponent } from './blog.component';
 import { BlogRoutingModule } from './blog-routing.module';
+import { BlogEntryComponent } from './components/blog-entry/blog-entry.component';
+import { BlogScrollerComponent } from './components/blog-scroller/blog-scroller.component';
 
 @NgModule({
-  declarations: [BlogComponent],
+  declarations: [BlogComponent, BlogEntryComponent, BlogScrollerComponent],
   imports: [CommonModule, BlogRoutingModule],
 })
 export class BlogModule {}
---
+++ src/app/modules/blog/components/blog-scroller/blog-scroller.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-scroller',
+  templateUrl: './blog-scroller.component.html',
+  styleUrls: ['./blog-scroller.component.scss']
+})
+export class BlogScrollerComponent {
+
+}
